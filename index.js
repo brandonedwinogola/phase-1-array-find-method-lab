e061edd9ee1a504659c7163cf8d0b30338b4472a
bran don <106029484+brandonedwinogola@users.noreply.github.com>
2022-06-11 15:55:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W" } { year: "2014", result: "N/A" } { year: "2013", result: "L" } { year: "2012", result: "N/A" },
+    //...
+]
+
+let object = superbowlWin() {
+    console.log(superbowlWin.windows(2015));
+
+    const sadReality = [{ result: N / A }; { result: N / A }; { result: N / A }]
+
+    superbowlWin = undefined
+}
